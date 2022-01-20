fff437cead9db2ba869de4d0510bb58e4b3632c2
KabutoDrip <cammiller42003@gmail.com>
2022-01-20 17:08:40 -0500

    Suit and value assignment

--- .vs/Hi-Lo/Cards/Hi-Lo.py
+++ .vs/Hi-Lo/Cards/Hi-Lo.py
@@ -4,22 +4,26 @@ import random
 class Deck:
     
     def __init__(self):
-        self.current = 0
-        self.points = 0
+        self.value = 0
+        self.suit = ''
         
     def draw(self):
         roll = random.randint(1,52)
         if roll in range (1,13):
             self.value = roll
+            self.suit = '\u2666'
         elif roll in range (14,26):
             self.value = roll - 13
+            self.suit = '\u2665'
         elif roll in range (27,39):
-            self.value = roll - 13
+            self.value = roll - 26
+            self.suit = '\u2663'
         elif roll in range (40,52):
-            self.value = roll - 13
+            self.value = roll - 39
+            self.suit = '\u2660'
         else:
             self.value = 0
+            self.suit = None
 
         
-print('\u2666', '\u2665', '\u2663', '\u2660')
 

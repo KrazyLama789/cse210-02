cc09ba10d646a76c19f0bac5b8ed31830daae887
Jenny <smilingjenny@gmail.com>
2022-01-21 13:49:07 -0700

    updated comments

--- cse210-02/Hi-Lo/game/director.py
+++ cse210-02/Hi-Lo/game/director.py
@@ -1,26 +1,21 @@
 from game.deck import Deck
 
-# To start with, I've copied the Director file from the dice came that we are allowed to use.
-
 class Director:
-    """A person who directs the game. 
-    
-    The responsibility of a Director is to control the sequence of play.
+    # The backbone of the program which controls the sequence of play.
 
-    Attributes:
-        deck (class): An instence of Deck
-        is_playing (boolean): Whether or not the game is being played.
-        initial_card (int): The starting face up card.
-        score (int): The score for one round of play.
-        score (int): The score for the entire game.
-    """
+    # Attributes:
+    #     deck (class): An instence of Deck
+    #     is_playing (boolean): Whether or not the game is being played.
+    #     initial_card (int, str): The starting face up card using the Deck method draw.
+    #     score (int): The score current score of the player.
+    #     guess (str): The players guess of higher or lower
+    #     draw (int, str): The face up card that's drawn after the player guesses
+    
 
     def __init__(self):
-        """Constructs a new Director.
+        # Constructs a new Director.
+        # Args: self (Director): an instance of Director.
         
-        Args:
-            self (Director): an instance of Director.
-        """
         self.deck = Deck()
         self.is_playing = True
         self.initial_card = self.deck.draw()
@@ -29,32 +24,26 @@ class Director:
         self.draw = 0
 
     def start_game(self):
-        """Starts the game by running the main game loop.
+        # Starts the game by running the main game loop.
+        # Args: self (Director): an instance of Director.
         
-        Args:
-            self (Director): an instance of Director.
-        """
         while self.is_playing:
             self.get_inputs()
             self.do_updates()
             self.do_outputs()
 
     def get_inputs(self):
-        """Ask the user to self.guess higher or lower.
-
-        Args:
-            self (Director): An instance of Director.
-        """
+        # Asks the player to guess if the next card will be higher or lower.
+        # Args: self (Director): An instance of Director.
+       
         print(f"\nThe card is: {self.initial_card}")
         self.guess = input("Higher or lower? [h/l] ")
         
     
     def do_updates(self):
-        """Updates the player's score.
-
-        Args:
-            self (Director): An instance of Director.
-        """
+        # Updates the player's score.
+        # Args: self (Director): An instance of Director.
+        
         self.draw = self.deck.draw()
         print(f"Next card is: {self.draw}") 
     
@@ -70,11 +59,9 @@ class Director:
                            
 
     def do_outputs(self):
-        """Displays the dice and the score. Also asks the player if they want to roll again. 
-
-        Args:
-            self (Director): An instance of Director.
-        """
+        # Displays the players score and asks if they want to play again. 
+        # Args: self (Director): An instance of Director.
+        
         if not self.is_playing:
             return
         
--- cse210-02/Hi-Lo/main.py
+++ cse210-02/Hi-Lo/main.py
@@ -1,5 +1,4 @@
 from game.director import Director
-# To start with, I've copied the main.py file from the dice came that we are allowed to use.
 
 director = Director()
 director.start_game()

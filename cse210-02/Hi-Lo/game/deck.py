0bb59bfc30a6042c39d2bd5bd6097184fc451c68
KabutoDrip <cammiller42003@gmail.com>
2022-01-21 16:30:47 -0500

    split suit selector and card drawing

--- cse210-02/Hi-Lo/game/deck.py
+++ cse210-02/Hi-Lo/game/deck.py
@@ -7,25 +7,25 @@ class Deck:
         self.value = 0
         self.suit = ''
         
-    def draw(self):
-        roll = random.randint(1,52)
-        if roll in range (1,13):
-            self.value = roll
-            self.suit = '\u2666'
-        elif roll in range (14,26):
-            self.value = roll - 13
-            self.suit = '\u2665'
-        elif roll in range (27,39):
-            self.value = roll - 26
-            self.suit = '\u2663'
-        elif roll in range (40,52):
-            self.value = roll - 39
-            self.suit = '\u2660'
-        # else:
-        #     self.value = 0
-        #     self.suit = None
+    
+    def draw_card(self):
+        #draw card with random number 1-13
+        print()
+        
+    def suit_selector(self): # method that generates and returns a random suit 
+        roll_suit = random.randint(1,4) 
 
-        return self.value
+        if roll_suit == 1:
+            self.suit = '\u2666'
+        elif roll_suit == 2:
+            self.suit = '\u2665'
+        elif roll_suit == 3:
+            self.suit = '\u2663'
+        elif roll_suit ==4:
+            self.suit = '\u2660'
+        return self.suit
+        
+        
 
         
 

b153dfe46562cf3f863b3a43e034f2468de4801b
Mary Goff <maryrgoff@gmail.com>
2022-01-21 15:39:17 -0700

    Added attributes and funcitonality for suit guessing.

--- Hi-Lo/game/director.py
+++ Hi-Lo/game/director.py
@@ -11,48 +11,49 @@ class Director:
     #     guess (str): The players guess of higher or lower
     #     draw (int, str): The face up card that's drawn after the player guesses
     
-
     def __init__(self):
         # Constructs a new Director.
         # Args: self (Director): an instance of Director.
         
         self.deck = Deck()
         self.is_playing = True
+        self.is_playing_suits = False
         self.initial_card = self.deck.draw()
         self.score = 300 # for starting value.
         self.guess = ''
         self.draw = 0
+        self.guess_suit = ''
+        self.draw_suit = ''
 
     def start_game(self):
         # Starts the game by running the main game loop.
         # Args: self (Director): an instance of Director.
         
-        suits = input('Do you want to play with suits? [y/n] ')
-        hard_mode = (suits == "y")
-        
         while self.is_playing:
             self.get_inputs()
-            if hard_mode == True:
-                self.do_suits
             self.do_updates()
             self.do_outputs()
 
     def get_inputs(self):
         # Asks the player to guess if the next card will be higher or lower.
         # Args: self (Director): An instance of Director.
+        
+        suits = input('Do you want to play with suits? [y/n] ')
+        self.is_playing_suits = (suits == "y")
        
         print(f"\nThe card is: {self.initial_card}")
         self.guess = input("Higher or lower? [h/l] ")
+        self.draw = self.deck.draw()
         
-    def do_suits(self):
-        pass       
+        if self.is_playing_suits == True:
+            self.guess_suit = input('Spade, clover, heart, or diamond? \n[s/c/h/d] ')
+            self.draw_suit = self.deck.suit_selector()
     
     def do_updates(self):
         # Updates the player's score.
         # Args: self (Director): An instance of Director.
         
-        self.draw = self.deck.draw()
-        print(f"Next card is: {self.draw}") 
+        print(f"Your card is: {self.draw} {self.draw_suit}") 
     
         if self.guess == "h" and self.draw >= self.initial_card:
             self.score += 100
@@ -64,7 +65,6 @@ class Director:
         if self.score <= 0:
             self.is_playing = False
                            
-
     def do_outputs(self):
         # Displays the players score and asks if they want to play again. 
         # Args: self (Director): An instance of Director.

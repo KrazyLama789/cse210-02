05d06648878169c43f4ad147968b2a1506610a67
KK <91944125+k-kameta@users.noreply.github.com>
2022-01-22 06:36:33 +0900

    Update deck.py
    updated deck file

--- cse210-02/Hi-Lo/game/deck.py
+++ cse210-02/Hi-Lo/game/deck.py
@@ -7,25 +7,15 @@ class Deck:
         self.value = 0
         self.suit = ''
         
-    def draw(self):
-        roll = random.randint(1,52)
-        if roll in range (1,13):
-            self.value = roll
-            self.suit = '\u2666'
-        elif roll in range (14,26):
-            self.value = roll - 13
-            self.suit = '\u2665'
-        elif roll in range (27,39):
-            self.value = roll - 26
-            self.suit = '\u2663'
-        elif roll in range (40,52):
-            self.value = roll - 39
-            self.suit = '\u2660'
-        # else:
-        #     self.value = 0
-        #     self.suit = None
+    def value(self):
+        roll = random.randint(1,13)
+        self.value = roll
 
         return self.value
-
         
+    def suite(self):
+        roll = random.randint(1,4)
+        self.suite = roll
+
+        return self.suite
 
